0e722eb0aec06a30b735382912f16c034a44acb6
Mirwais Hotak <mirwais.hotak@nordakademie.de>
2020-01-07 17:45:53 +0100

    New Classes Added

---
+++ src/main/java/de/nordakademie/informaticup/pandemicfighter/gameengine/DecisionMaker.java
@@ -0,0 +1,17 @@
+package de.nordakademie.informaticup.pandemicfighter.gameengine;
+
+public class DecisionMaker {
+
+    public boolean isActionToBeTakenCity (double cityThreat, double pathogenThreat){
+        boolean takeAction = false;
+
+        return takeAction;
+    }
+
+    public boolean isActionToBeTakenGlobal(double pahtogenThreat){
+        boolean takeAction = false;
+
+        return takeAction;
+    }
+
+}
--- src/main/java/de/nordakademie/informaticup/pandemicfighter/gameengine/ThreatEvaluator.java
+++ src/main/java/de/nordakademie/informaticup/pandemicfighter/gameengine/ThreatEvaluator.java
@@ -1,4 +1,29 @@
 package de.nordakademie.informaticup.pandemicfighter.gameengine;
 
+import de.nordakademie.informaticup.pandemicfighter.gameengine.elements.City;
+import de.nordakademie.informaticup.pandemicfighter.gameengine.elements.events.Event;
+import de.nordakademie.informaticup.pandemicfighter.gameengine.elements.events.OutbreakEvent;
+
+import java.util.ArrayList;
+
+import static de.nordakademie.informaticup.pandemicfighter.gameengine.ThreatIndicator.getPathogenThreatIndicatore;
+
 public class ThreatEvaluator {
+
+    public double calculateForPutUnderQurantine(City city){
+        double threat = 1;
+        double threatCity = ThreatIndicator.getCityThreatIndicator(city);
+        double threatPathogen;
+        ArrayList<Event> events = city.getEvents();
+        for(Event event: events){
+            if("outbreak".equals(event.getType())){
+                OutbreakEvent outbreakEvent = (OutbreakEvent) event;
+               threatPathogen = ThreatIndicator.getPathogenThreatIndicatore(outbreakEvent.getPathogen());
+
+            }
+        }
+
+        return threat;
+    }
+
 }
