6ab17c9f97592c6a44b819de3cd7eddfd819c4a0
Danial Mirrezaei <mirrezaei@t-online.de>
2020-01-08 12:26:27 +0100

    Implemented DecisionMaker Closes #21

--- src/main/java/de/nordakademie/informaticup/pandemicfighter/gameengine/ActionSelector.java
+++ src/main/java/de/nordakademie/informaticup/pandemicfighter/gameengine/ActionSelector.java
@@ -2,10 +2,7 @@ package de.nordakademie.informaticup.pandemicfighter.gameengine;
 
 import com.google.gson.JsonObject;
 import de.nordakademie.informaticup.pandemicfighter.gameengine.actions.Action;
-import de.nordakademie.informaticup.pandemicfighter.gameengine.provider.JsonActionProvider;
-import de.nordakademie.informaticup.pandemicfighter.gameengine.elements.Pathogen;
-import de.nordakademie.informaticup.pandemicfighter.gameengine.elements.events.PathogenEncounteredEvent;
-import de.nordakademie.informaticup.pandemicfighter.gameengine.provider.cabinets.VaccineCabinet;
+import de.nordakademie.informaticup.pandemicfighter.gameengine.actions.EndRoundAction;
 
 import java.util.ArrayList;
 
@@ -17,19 +14,10 @@ public class ActionSelector {
     }
 
     public JsonObject getAction() {
-        JsonObject action = JsonActionProvider.endRound();
-        boolean noOtherAction = true; // TODO: tbd
-        if (!"pending".equals(game.getOutcome()) || noOtherAction) {
-            PathogenEncounteredEvent pathogenEvent = (PathogenEncounteredEvent) game.getEvents().get(0);
-            Pathogen pathogen = pathogenEvent.getPathogen();
-            int roundsUntilVaccineIsAvailable = VaccineCabinet.roundsUntilVaccineIsAvailable(pathogen.getName());
-            if (roundsUntilVaccineIsAvailable == -1) {
-                action = JsonActionProvider.developVaccine(pathogen);
-            } else {
-                action = JsonActionProvider.endRound();
-            }
+        JsonObject action = new EndRoundAction().toJson();
+        if ("pending".equals(game.getOutcome())) {
             ArrayList<Action> actions = new ActionCreator().getAllPossibleActions(game);
-            System.out.println("Vaccine status: " + roundsUntilVaccineIsAvailable);
+            action = new DecisionMaker().getBestAction(actions).toJson();
         }
         return action;
     }
--- src/main/java/de/nordakademie/informaticup/pandemicfighter/gameengine/DecisionMaker.java
+++ src/main/java/de/nordakademie/informaticup/pandemicfighter/gameengine/DecisionMaker.java
@@ -1,17 +1,36 @@
 package de.nordakademie.informaticup.pandemicfighter.gameengine;
 
+import de.nordakademie.informaticup.pandemicfighter.gameengine.actions.Action;
+
+import java.util.ArrayList;
+
 public class DecisionMaker {
+    private static final double FACTOR_POINT = 1.05;
 
-    public boolean isActionToBeTakenCity (double cityThreat, double pathogenThreat){
-        boolean takeAction = false;
-
-        return takeAction;
+    public Action getBestAction(ArrayList<Action> actions) {
+        Action bestAction = actions.get(0);
+        for (int i = 1; i < actions.size(); i++) {
+            bestAction = getBetterAction(bestAction, actions.get(i));
+        }
+        return bestAction;
     }
 
-    public boolean isActionToBeTakenGlobal(double pahtogenThreat){
-        boolean takeAction = false;
-
-        return takeAction;
+    private Action getBetterAction(Action action1, Action action2) {
+        Action betterAction;
+        double action1ThreatIndicator = action1.getThreatIndicator();
+        double action2ThreatIndicator = action2.getThreatIndicator();
+        if (action1.getPoints() < action2.getPoints()) {
+            int pointDifference = action2.getPoints() - action1.getPoints();
+            action1ThreatIndicator *= Math.pow(FACTOR_POINT, pointDifference);
+        } else if (action1.getPoints() > action2.getPoints()) {
+            int pointDifference = action1.getPoints() - action2.getPoints();
+            action2ThreatIndicator *= Math.pow(FACTOR_POINT, pointDifference);
+        }
+        if (action1ThreatIndicator >= action2ThreatIndicator) {
+            betterAction = action1;
+        } else {
+            betterAction = action2;
+        }
+        return betterAction;
     }
-
 }
